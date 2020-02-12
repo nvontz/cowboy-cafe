6d238f81de46a74e7f2f171e0dc4c5dd5ffb5ecd
Nathan Vontz <nvontz@ksu.edu>
2020-02-12 14:41:34 -0600

    started 3

---
+++ Data/Drink.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    public abstract class Drink
+    {
+        public abstract double Price { get; }
+
+        public abstract uint Calories { get; }
+
+        public abstract List<string> SpecialInstructions { get; }
+
+        public virtual Size Size { get; set; }
+
+        public abstract bool Ice 
+
+    }
+}
