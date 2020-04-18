be28758739970f6cfb616bc8a506fcbe6f7455e2
Nathan Vontz <nvontz@ksu.edu>
2020-04-17 22:05:55 -0500

    need to put into columns

---
+++ Data/Menu.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CowboyCafe.Data
+{
+    public static class Menu
+    {
+        public static IEnumerable<IOrderItem> Entrees()
+        {
+            List<IOrderItem> entrees = new List<IOrderItem>();
+           
+            entrees.Add(new AngryChicken());
+            entrees.Add(new CowpokeChili());
+            entrees.Add(new DakotaDoubleBurger());
+            entrees.Add(new PecosPulledPork());
+            entrees.Add(new RustlersRibs());
+            entrees.Add(new TrailBurger());
+            entrees.Add(new TexasTripleBurger());
+
+            return entrees.ToArray();
+        }
+
+        public static IEnumerable<IOrderItem> Sides()
+        {
+            List<IOrderItem> sides = new List<IOrderItem>();
+
+            sides.Add(new ChiliCheeseFries());
+            sides.Add(new CornDodgers());
+            sides.Add(new PanDeCampo());
+            sides.Add(new BakedBeans());
+
+            return sides.ToArray();
+        }
+
+        public static IEnumerable<IOrderItem> Drinks()
+        {
+            List<IOrderItem> drinks = new List<IOrderItem>();
+
+            drinks.Add(new JerkedSoda());
+            drinks.Add(new TexasTea());
+            drinks.Add(new CowboyCoffee());
+            drinks.Add(new Water());
+
+            return drinks.ToArray();
+        }
+
+        public static IEnumerable<IOrderItem> CompleteMenu()
+        {
+            List<IOrderItem> menu = new List<IOrderItem>();
+
+            menu.Add(new AngryChicken());
+            menu.Add(new CowpokeChili());
+            menu.Add(new DakotaDoubleBurger());
+            menu.Add(new PecosPulledPork());
+            menu.Add(new RustlersRibs());
+            menu.Add(new TrailBurger());
+            menu.Add(new TexasTripleBurger());
+
+            menu.Add(new ChiliCheeseFries());
+            menu.Add(new CornDodgers());
+            menu.Add(new PanDeCampo());
+            menu.Add(new BakedBeans());
+
+            menu.Add(new JerkedSoda());
+            menu.Add(new TexasTea());
+            menu.Add(new CowboyCoffee());
+            menu.Add(new Water());
+
+            return menu.ToArray();
+        }
+    }
+}
